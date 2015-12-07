a76175dea7b1de4cfdfce8a0ab68d8359a880ae8
k2works <kakimomokuri@gmail.com>
2015-12-07 13:22:36 +0900

    タスクの作成/編集/削除機能の実装

--- lib/application.rb
+++ lib/application.rb
@@ -9,7 +9,22 @@ require 'todo/task'
 module Todo
   class Application < Sinatra::Base
 
+    use Rack::MethodOverride
+
     set :haml, escape_html: true
+
+    helpers do
+      def error_class(task, name)
+        task.errors.has_key?(name) ? 'has-error':''
+      end
+
+      def options_for_task_status(task)
+        Task::STATUS.map { |key, value|
+          selected = (value == task.status) ? ' selected' : ''
+          %(<option value="#{value}"#{selected}>#{key}</option>)
+        }.join
+      end
+    end
 
     configure do
       DB.prepare
@@ -42,5 +57,66 @@ module Todo
       haml :index
     end
 
+    get '/tasks/new' do
+      @task = Task.new
+
+      haml :new
+    end
+
+    post '/tasks' do
+      begin
+        Task.create!(name: params[:name], content: params[:content])
+
+        redirect '/'
+      rescue ActiveRecord::RecordInvalid => e
+        @task = e.record
+
+        haml :new
+      end
+    end
+
+    get '/tasks/:id/edit' do
+      begin
+        @task = Task.find(params[:id])
+
+        haml :edit
+      rescue ActiveRecord::RecordNotFound
+        error 404
+      end
+    end
+
+    put '/tasks/:id' do
+      begin
+        task = Task.find(params[:id])
+        task.update_attributes!(
+                name:    params[:name],
+                content: params[:content],
+                status:  params[:status]
+        )
+
+        redirect '/'
+      rescue ActiveRecord::RecordInvalid => e
+        @task = e.record
+
+        haml :edit
+      rescue ActiveRecord::RecordNotFound
+        error 4040
+      end
+    end
+
+    delete '/tasks/:id' do
+      begin
+        task = Task.find(params[:id])
+        task.destroy
+
+        redirect '/'
+      rescue ActiveRecord::RecordNotFound
+        error 4040
+      end
+    end
+
+    not_found do
+      haml :not_found
+    end
   end
 end
