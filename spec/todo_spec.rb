2b8ae5965ed512306281ded0ab2a8e9801d33ac8
k2works <kakimomokuri@gmail.com>
2016-02-12 16:42:06 +0900

    Step05

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -5,31 +5,29 @@ describe Todo do
     expect(Todo::VERSION).not_to be nil
   end
 
-  describe 'DB' do
-    it 'やることが登録できる' do
+  describe 'Task' do
+    before(:each) do
       Todo::DB.prepare
-      params = {name:'test',content:'contents'}
-      Todo::Task.where(params).delete_all
-      Todo::Task.create!(name: params[:name], content: params[:content])
+      @params = {name:'test',content:'contents'}
+      Todo::Task.create!(name: @params[:name], content: @params[:content])
+    end
+
+    after(:each) do
+      Todo::Task.where(@params).delete_all
+    end
+
+    it 'やることが登録できる' do
       expect(Todo::Task.all.count).to eq(1)
     end
 
     it 'やることが検索できる' do
-      Todo::DB.prepare
-      params = {name:'test',content:'contents'}
-      Todo::Task.where(params).delete_all
-      Todo::Task.create!(name: params[:name], content: params[:content])
-      id = Todo::Task.where(params)[0].id
+      id = Todo::Task.where(@params)[0].id
       task = Todo::Task.find(id)
       expect(task.name).to eq('test')
     end
 
     it 'やることが更新できる' do
-      Todo::DB.prepare
-      params = {name:'test',content:'contents'}
-      Todo::Task.where(params).delete_all
-      Todo::Task.create!(name: params[:name], content: params[:content])
-      id = Todo::Task.where(params)[0].id
+      id = Todo::Task.where(@params)[0].id
       task = Todo::Task.find(id)
       task.name = 'test2'
       task.save
@@ -38,11 +36,9 @@ describe Todo do
     end
 
     it 'やることが削除できる' do
-      Todo::DB.prepare
-      params = {name:'test',content:'contents'}
-      Todo::Task.where(params).delete_all
-      Todo::Task.create!(name: params[:name], content: params[:content])
-      Todo::Task.where(params).delete_all
+      id = Todo::Task.where(@params)[0].id
+      task = Todo::Task.find(id)
+      task.destroy
       expect(Todo::Task.all.count).to eq(0)
     end
   end
